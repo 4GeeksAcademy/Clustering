ea5243388106560138e4f98652833539ab838ddb
Andrea <97256300+xatisba@users.noreply.github.com>
2024-08-09 20:17:39 +0200

    Clustering Completed

--- src/app.py
+++ src/app.py
@@ -1,4 +1,17 @@
-from utils import db_connect
-engine = db_connect()
+import pandas as pd
+import numpy as np
 
-# your code here
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import ssl
+import urllib.request
+
+url = 'https://raw.githubusercontent.com/4GeeksAcademy/k-means-project-tutorial/main/housing.csv'
+
+ssl_context = urllib.request.urlopen(url, context=ssl._create_unverified_context())
+
+df = pd.read_csv(ssl_context, index_col=None)
+
+df.to_csv('/Users/andreawendezflores/Documents/GitHub/Clustering/data/raw/california_housing_data', index=False)
+
